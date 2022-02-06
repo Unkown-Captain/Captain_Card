cfb10c43b0a0104991d5a8ff2f50ab0413ed912d
Unkown-Captain <76992346+Unkown-Captain@users.noreply.github.com>
2022-02-06 16:31:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -23,13 +23,16 @@
         <a href="https://www.facebook.com/profile.php?id=100015256921088" target="_blank" style="color: inherit;"><i class="fab fa-facebook-f"></i></a>
         <a href="https://www.instagram.com/invites/contact/?i=1gm402cmibalm&utm_content=39fdewa" target="_blank" style="color: inherit;"><i class="fab fa-instagram"></i></a>
         <a href="https://youtube.com/channel/UCA44ghvYkWK07I9uDwYrgMw " target="_blank" style="color: inherit;"><i class="fab fa-youtube"></i></a>
-        <a href="https://twitter.com/AbdulWa65813238?s=08" target="_blank" style="color: inherit;"><i class="fab fa-twitter"></i></a>
+        <a href="https://twitter.com/captainWaheed43?t=zwtKnD18_68apudbyFZM1A&s=09" target="_blank" style="color: inherit;"><i class="fab fa-twitter"></i></a>
       </div>
     </div>
 </body>
 <footer>
   <div class="attribution"> 
-    <p>Copyright 2022 Ⓒ <span>Waheed's Template</span>. All Rights Reserved.</p> 
+    <p>
+        Copyright 2021 Ⓒ <span><a href="#">Waheed's Template</a> </span>. All
+        Rights Reserved.
+      </p>
   </div>
 </footer>
 </html>
